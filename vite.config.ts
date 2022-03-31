700ef7c5663f87fa624589faa292d00fcae8b118
buer <hchi404@gmail.com>
2022-03-31 23:48:48 +0800

    add unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite"
 import path from "path"
 import Unocss from "unocss/vite"
 import react from "@vitejs/plugin-react"
+import AutoImport from "unplugin-auto-import/vite"
 
 export default defineConfig({
   base: "./",
@@ -10,5 +11,12 @@ export default defineConfig({
       "~/": `${path.resolve(__dirname, "src")}/`,
     },
   },
-  plugins: [react(), Unocss()],
+  plugins: [
+    react(),
+    Unocss(),
+    AutoImport({
+      imports: ["react"],
+      dts: "src/auto-imports.d.ts",
+    }),
+  ],
 })

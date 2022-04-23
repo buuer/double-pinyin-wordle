65fbc7b22877f9e6b56205b0cc109866ab42f166
buer <hchi404@gmail.com>
2022-04-23 18:40:21 +0800

    chore: unocss color theme

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,11 +1,22 @@
-import {
-  defineConfig,
-  presetIcons,
-  presetTypography,
-  presetWind,
-} from "unocss"
+import { defineConfig, presetIcons, presetTypography, presetWind } from "unocss"
+import transformerVariantGroup from "@unocss/transformer-variant-group"
+import transformerDirective from "@unocss/transformer-directives"
 
 export default defineConfig({
-  shortcuts: [],
+  shortcuts: [["activatable", "cursor-pointer active:opacity-65"]],
+  theme: {
+    colors: {
+      wordle: {
+        green: "#6aaa64",
+        darkendGreen: "#538d4e",
+        yellow: "#c9b458",
+        darkendYellow: "#b59f3b",
+        lightGray: "#d8d8d8",
+        gray: "#86888a",
+        darkGray: "#939598",
+      },
+    },
+  },
   presets: [presetWind(), presetIcons(), presetTypography()],
+  transformers: [transformerVariantGroup(), transformerDirective()],
 })

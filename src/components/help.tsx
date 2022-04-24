c68e5af520b6f92178ca59af274d6d9db3bacc32
buer <hchi404@gmail.com>
2022-04-24 23:00:21 +0800

    refactor: Help

--- src/components/help.tsx
+++ src/components/help.tsx
@@ -1,99 +1,85 @@
-import { FunctionalComponent } from 'react'
-import { useWordleContext } from '../utils/reduce'
-import Popup from './popup'
-import './help.scss'
-import { BoardRow } from './board'
-import { useCallback, useEffect, useState } from 'react'
+import Popup from "./popup"
+import { BoardRow } from "./board"
+import { useWordleContext } from "~/state/context"
 
-const Link: FunctionalComponent<{ href: string }> = (props) => (
-  <a class="help-a" rel="noreferrer" target="_blink" href={props.href}>
+const Link: FC<{ href: string; children: ReactNode }> = (props) => (
+  <a
+    className="m-1 underline"
+    rel="noreferrer"
+    target="_blink"
+    href={props.href}
+  >
     {props.children}
   </a>
 )
 
-const getRowData = (keys: string, status: Record<string, number>) =>
-  keys.split(' ').map((k, idx) => [k, status[idx] || 0] as [string, number])
-
-const Help: FunctionalComponent = () => {
-  const { emit, state } = useWordleContext()
-  const [statusDely, setStatusDely] = useState(false)
-
-  useEffect(() => {
-    requestAnimationFrame(() => setStatusDely(state.showHelp))
-  }, [state.showHelp])
+const Help: FC = () => {
+  const { modify, state } = useWordleContext()
 
   const handleClose = useCallback(() => {
-    emit('setState', { showHelp: false })
-  }, [emit])
+    modify("modal", null)
+  }, [modify])
 
   return (
-    <Popup show={state.showHelp} onMaskClick={handleClose}>
-      <div className="help">
-        <h1 className="help-h1">
-          怎么玩
-          <span className="help-close" onClick={handleClose}>
-            &#x2715;
+    <Popup
+      show={state.modal === "help"}
+      onMaskClick={handleClose}
+      className="inset-0 p-4 c-#c0c4c3"
+    >
+      <h1 className="text-2xl font-600" onClick={handleClose}>
+        怎么玩
+      </h1>
+
+      <div className="help-content all-[p]-my-2">
+        <p>六次机会猜成语，填满一行按 🕹 </p>
+        <p>声母/韵母颜色，提供猜词线索</p>
+        <p className="border-b border-light border-opacity-30" />
+        <p>例：</p>
+        <p>
+          <span className="inline-block mr-2 w-6 h-6 text-center bg-wordle-green">
+            绿
           </span>
-        </h1>
+          <span className="inline-block mr-2 c-wordle-green">L</span>
+          正确
+        </p>
+        <BoardRow />
 
-        <div className="help-scroll">
-          <p>六次机会猜成语，填满一行按 🕹 </p>
-          <p>声母/韵母颜色，提供猜词线索</p>
-          <div className="help-hr" />
-          <p>例：</p>
-          <div className="help-board board">
-            <BoardRow
-              rowData={getRowData(
-                'l v u v q k u j',
-                statusDely ? { 0: 1 } : {}
-              )}
-            />
-          </div>
-          <p>
-            <span className="with-status correct">L</span>本格正确
-          </p>
-          <div className="help-board board">
-            <BoardRow
-              rowData={getRowData(
-                'f w h l t g d a',
-                statusDely ? { 3: 2 } : {}
-              )}
-            />
-          </div>
-          <p>
-            <span className="with-status present">iang/uang</span>位置错误
-          </p>
-          <div className="help-board board">
-            <BoardRow
-              rowData={getRowData(
-                'd m d c h w b d',
-                statusDely ? { 4: 3 } : {}
-              )}
-            />
-          </div>
-          <p>
-            <span className="with-status absent">H</span>成语中没有用到该拼音
-          </p>
-          <div className="help-hr" />
-          <p>每天都有一个新的成语</p>
+        <p>
+          <span className="inline-block mr-2 w-6 c-wordle-yellow">黄</span>
+          <span className="inline-block mr-2 c-wordle-yellow">iang/uang</span>
+          位置错误
+        </p>
+        <BoardRow />
+        <p>
+          <span className="inline-block mr-2 c-wordle-gray">H</span>
+          成语中没有用到该拼音/字
+        </p>
+        <BoardRow />
 
-          <div className="help-links">
-            <p>
-              <Link href="https://www.powerlanguage.co.uk/wordle/">WORDLE</Link>
-              原版
-            </p>
-            <p>
-              韵母输入使用
-              <Link href="https://help.flypy.com/#/up">小鹤双拼</Link>
-              的映射规则
-            </p>
-            <p>
-              本项目托管于
-              <Link href="https://github.com/buuer/double-pinyin-wordle/">
-                github
-              </Link>
-            </p>
-          </div>
+        <p className="border-b border-light border-opacity-30" />
+        <p>每天都有一个新的成语</p>
+
+        <div
+          className={classnames(
+            "start text-center my-4 ",
+            "border-1 border-light border-opacity-30 rounded-1 py-2",
+            "activatable"
+          )}
+          onClick={handleClose}
+        >
+          开始吧
+        </div>
+        <div className="mt-4 text-right opacity-60 text-sm">
+          <p>
+            原版
+            <Link href="https://www.powerlanguage.co.uk/wordle/">WORDLE</Link>
+          </p>
+          <p>
+            本项目托管于
+            <Link href="https://github.com/buuer/double-pinyin-wordle/">
+              github
+            </Link>
+          </p>
         </div>
       </div>
     </Popup>

3fe92258084ae2b23093bc5638ae283df1c8be71
buer <hchi404@gmail.com>
2022-04-24 21:49:25 +0800

    board 样式布局及不同尺寸适配

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -1,17 +1,10 @@
+import { STATUS } from "~/utils/counst"
+import { addTone, removeTone } from "~/utils/pinyin"
+import { status } from "~/utils/types"
 import { indexArray, randomNum } from "../utils/func"
 
 const ROW_LEN = 6
 const COL_LEN = 4
-
-const STATUS = {
-  BASE: 0,
-  GRAY: 1,
-  YELLOW: 2,
-  GREEN: 3,
-}
-
-type statusMap = typeof STATUS
-type status = statusMap[keyof statusMap]
 
 const Loop: FC<{
   node: (index: number) => ReactNode
@@ -21,49 +14,80 @@ const Loop: FC<{
 }
 
 const statusClass = (status: status) => ({
-  "c-wordle-green": status === STATUS.GREEN,
-  "c-wordle-yellow": status === STATUS.YELLOW,
-  "c-wordle-gray": status === STATUS.GRAY,
+  "c-wordle-green": status === STATUS.CORRENT,
+  "c-wordle-yellow": status === STATUS.PRESENT,
+  "c-wordle-gray": status === STATUS.ABSENT,
 })
 
 const BoardCell: FC<{
-  value?: [string, string, string]
-  status?: [status, status, status]
+  value?: [string, string, string, number]
+  status?: [status, status, status, status]
 }> = (props) => {
   const { value, status } = props
-  const [statusHan, statusSheng, statusYun] = status || [
+  const [statusHan, statusSheng, statusYun, statusTone] = status || [
+    STATUS.BASE,
     STATUS.BASE,
     STATUS.BASE,
     STATUS.BASE,
   ]
 
-  const [han, sheng, yun] = value || ["", "", ""]
+  const [han, sheng, yun, tone] = value || ["", "", "", 0]
 
   return (
     <div
       className={classnames(
-        "board-cell grid-cols-1 grid w-16 h-16 flex-none",
-        "border-1 mr-[-1px] border-red-200",
-        ""
+        "board-cell relative pt-2 text-center",
+        "lt-mxs:w-16 lt-mxs:h-16",
+        "at-mxs:w-18 at-mxs:h-18",
+        "at-msm:w-20 at-msm:h-20",
+        "at-mmd:w-22 at-mmd:h-22",
+        "mlg:w-24 mlg:h-24",
+        "flex flex-none items-center justify-center",
+        "border-1 mr-[-1px] border-#ed5126",
+        statusHan === STATUS.CORRENT
+          ? "c-white bg-wordle-green all-[.status-color]-c-white"
+          : "c-#310f1b"
       )}
     >
-      <div className="text-center  border-b border-light">
-        <span className={classnames("text-sm", statusClass(statusSheng))}>
+      <div
+        className={classnames(
+          "text-base",
+          "absolute top-0 w-full",
+          "border-b border-white border-opacity-10"
+        )}
+      >
+        <span
+          className={classnames(
+            statusClass(statusSheng),
+            "status-color uppercase"
+          )}
+        >
           {sheng}
         </span>
-        <span className={classnames("text-sm", statusClass(statusYun))}>
-          {yun}
+        <span className={"relative"}>
+          <span
+            className={classnames(
+              "status-color",
+              statusClass(statusTone),
+              "absolute left-0 z--1"
+            )}
+          >
+            {addTone(yun, tone)}
+          </span>
+          <span className={classnames(statusClass(statusYun), "status-color")}>
+            {removeTone(yun)}
+          </span>
         </span>
       </div>
-      <span
+      <div
         className={classnames(
-          "col-span-3 pb-1",
-          "text-center text-2xl font-serif",
-          statusClass(statusHan)
+          statusClass(statusHan),
+          "status-color",
+          "text-3xl font-serif"
         )}
       >
         {han}
-      </span>
+      </div>
     </div>
   )
 }
@@ -74,16 +98,19 @@ export const BoardRow: FC<{
 }> = (props) => {
   return (
     <div
-      className={classnames("board-row flex justify-center items-center my-2", {
-        shaking: Boolean(props.shaking),
-      })}
+      className={classnames(
+        "board-row flex justify-center items-center mt--1px",
+        {
+          shaking: Boolean(props.shaking),
+        }
+      )}
     >
       <Loop
         node={(index) => (
           <BoardCell
             key={index}
-            status={[randomNum(3), randomNum(3), randomNum(3)]}
-            value={["望", "W", "ang"]}
+            status={[randomNum(3), randomNum(3), randomNum(3), randomNum(3)]}
+            value={["帽", "m", "ào", 4]}
           />
         )}
         length={COL_LEN}

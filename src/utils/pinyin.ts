db2463b16170909a8e449302f9c7550e1e61b0e5
buer <hchi404@gmail.com>
2022-05-11 01:10:09 +0800

    fix: i -> ı 方便显示声调

--- src/utils/pinyin.ts
+++ src/utils/pinyin.ts
@@ -8,7 +8,7 @@ export const splitTone = (pinyin: string = "") => {
     .map((c) => {
       if (TONES_MAP[c]) {
         tone = TONES_MAP[c][1]
-        return TONES_MAP[c][0]
+        return tone ? TONES_MAP[c][0] : c
       }
       return c
     })
@@ -23,7 +23,7 @@ export const addTone = (pinyin: string, tone: number) => {
 export const wordToPinyin = (word: string) =>
   toPinyin(word, { style: toPinyin.STYLE_TONE }).map(([pinyin]) => {
     const [withoutTone, tone] = splitTone(pinyin)
-    const [p1, p2] = withoutTone.split(/(?=[aeiou])/)
+    const [p1, p2] = withoutTone.split(/(?=[aeiıouü])/)
     const sheng = p2 ? p1 : ""
     const yun = pinyin.replace(sheng, "")
     return [sheng, yun, tone] as const
